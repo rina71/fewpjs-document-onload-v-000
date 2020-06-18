8897eaee0ce8d9263780f6484665c83fa0523908
Yurina Shin <yurinas@gmail.com>
2020-06-18 03:29:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ document.addEventListener( "DOMContentLoaded", function () {
 
 
 function updateDOM() {
-  document.getElementById( "text" )
-    .innerHTML = "This is really cool!";
+  document.getElementById( "text" ).innerHTML = "This is really cool!";
 }
